f2897a9e2f8dfcc4dad74d6123e44977b276fd8e
Danny L <dllenselink@gmail.com>
2022-10-18 10:10:34 -0500

    Added flattenData function

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,27 @@ import axios from 'axios';
 
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 
-export { fetcher };
+const flattenVehicleMakeData = (data) =>
+  data.map(({ makeName, vehicleModelList }) => ({
+    make: makeName,
+    ...vehicleModelList.reduce(
+      (accumModel, { vehicleModelName, vehicleList }) => ({
+        ...accumModel,
+        name: vehicleModelName,
+        ...vehicleList.reduce(
+          (accumVehicle, { id, year, vin, color, purchasePrice }) => ({
+            ...accumVehicle,
+            id,
+            year,
+            vin,
+            color,
+            purchasePrice,
+          }),
+          {}
+        ),
+      }),
+      {}
+    ),
+  }));
+
+export { fetcher, flattenVehicleMakeData };
